e9ce479be2a6c708913643dcc474134dd095c84d
Lucian Anghel <74255237+Anghel-Lucian@users.noreply.github.com>
2021-08-17 12:14:44 +0300

    Update Flashcard.js

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -4,32 +4,6 @@ import { decodeString } from '../apis/opentdb';
 const Flashcard = ({ question }) => {
   const [flip, setFlip] = useState(false);
 
-  const cardFront = useRef();
-  const cardBack = useRef();
-
-  const onFlip = () => {
-    if (!flip) {
-      return (
-        <div className="front" ref={cardFront}>
-          <div className="question">{decodeString(question.question)}</div>
-          <div className="possible-answers">
-            {[...question.incorrect_answers, question.correct_answer].map(
-              (answer) => (
-                <p className="possible-answer">{decodeString(answer)}</p>
-              )
-            )}
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="back" ref={cardBack}>
-          <div className="answer">{decodeString(question.correct_answer)}</div>
-        </div>
-      );
-    }
-  };
-
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
@@ -37,7 +11,7 @@ const Flashcard = ({ question }) => {
         setFlip(!flip);
       }}
     >
-      <div className="front" ref={cardFront}>
+      <div className="front">
         <div className="question">{decodeString(question.question)}</div>
         <div className="possible-answers">
           {[...question.incorrect_answers, question.correct_answer].map(
@@ -47,7 +21,7 @@ const Flashcard = ({ question }) => {
           )}
         </div>
       </div>
-      <div className="back" ref={cardBack}>
+      <div className="back">
         <div className="answer">{decodeString(question.correct_answer)}</div>
       </div>
     </div>
